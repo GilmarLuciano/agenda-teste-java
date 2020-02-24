c42ec506dd43b4f16c22a36f8711cdc0c477b9c5
@GilmarLuciano <gilmarluciano@yahoo.com.br>
2020-02-24 11:08:50 -0300

    Projeto criado

---
+++ src/main/java/com/teste/agenda/Agenda1Application.java
@@ -0,0 +1,13 @@
+package com.teste.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agenda1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agenda1Application.class, args);
+	}
+
+}
